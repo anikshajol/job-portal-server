f35a897a0ebf60a6a4ff1174761c342f8bf67610
anik shajol <anikshajol@gmail.com>
2026-02-05 20:55:12 +0600

    jwt setup learning done

--- index.js
+++ index.js
@@ -4,23 +4,22 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const app = express();
+const admin = require("firebase-admin");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"], // your frontend
-    credentials: true, //allow cookies
+    origin: ["http://localhost:5173"],
+    credentials: true,
   }),
 );
 app.use(express.json());
 app.use(cookieParser());
 
 const verifyToken = (req, res, next) => {
-  const token = req?.cookies?.accessToken;
-
-  console.log("cookies in the middleware");
+  const token = req?.cookies?.token;
   if (!token) {
     return res.status(401).send({ message: "Unauthorized access" });
   }
@@ -29,9 +28,38 @@ const verifyToken = (req, res, next) => {
     if (err) {
       return res.status(401).send({ message: "Unauthorized access" });
     }
-    console.log(decoded);
+
+    // console.log(token);
+    req.decoded = decoded;
+    console.log(req.decoded);
+
+    next();
   });
+};
+
+const serviceAccount = require("./job-portal-9e852-firebase-adminsdk-fbsvc-91e93ae634.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// firebaseToken verifyToken;
+
+const firebaseVerifyToken = async (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  console.log(token);
+
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  const userInfo = await admin.auth().verifyIdToken(token);
+
+  req.tokenEmail = userInfo.email;
+
   next();
+
+  // console.log("inside firebase token", userInfo);
 };
 
 // mongodb
@@ -55,41 +83,45 @@ async function run() {
       .collection("applications");
 
     // jsonwebtoken
-
     app.post("/jwt", (req, res) => {
-      const { email } = req.body;
-      const user = { email };
-      // console.log(userInfo);
-
-      const token = jwt.sign(user, process.env.JWT_SECRET_KEY, {
+      const userData = req.body;
+      const token = jwt.sign(userData, process.env.JWT_SECRET_KEY, {
         expiresIn: "1h",
       });
 
-      res.cookie("accessToken", token, {
+      res.cookie("token", token, {
         httpOnly: true,
         secure: false,
-        // sameSite: "lax",
-        maxAge: 60 * 60 * 1000,
+        sameSite: "lax",
+        maxAge: 60 * 60 * 1000, //
       });
-
       res.send({ success: true });
     });
 
     // app.post("/jwt", (req, res) => {
     //   const { email } = req.body;
     //   const user = { email };
+    //   // console.log(userInfo);
 
     //   const token = jwt.sign(user, process.env.JWT_SECRET_KEY, {
     //     expiresIn: "1h",
     //   });
 
-    //   res.send({ token });
+    //   res.cookie("accessToken", token, {
+    //     httpOnly: true,
+    //     secure: false,
+    //     sameSite: "lax",
+    //     maxAge: 60 * 60 * 1000,
+    //   });
+
+    //   res.send({ success: true });
     // });
 
     // get jobs
 
     app.get("/jobs", async (req, res) => {
       const email = req.query.email;
+      // console.log(email);
       const query = {};
       if (email) {
         query.hr_email = email;
@@ -120,11 +152,18 @@ async function run() {
 
     // get
 
-    app.get("/applications", verifyToken, async (req, res) => {
+    app.get("/applications", firebaseVerifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
+
+      if (email !== req.tokenEmail) {
+        return res.status(403).send({ message: "forbidden" });
+      }
+      // if (email !== req.decoded.email) {
+      //   return res.status(403).send({ message: "forbidden" });
+      // }
       const result = await applicationsCollection.find(query).toArray();
-      // console.log("inside application api", req.cookies);
+      console.log("inside application api", req.cookies);
 
       // eta hoche data arek collection er document theke ana, eta valo way na,
       for (let application of result) {
